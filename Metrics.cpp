d9bb3ffe8ad4ccae8bbb6f31a8bee1234c692c21
tiirth22 <152633332+tiirth22@users.noreply.github.com>
2024-03-12 09:42:53 +0530

    Update Metrics.cpp

--- Metrics.cpp
+++ Metrics.cpp
@@ -1,3 +1,14 @@
+/*This GitHub code has been crafted by TIRTH JIGNESHKUMAR DALAL from CHARUSAT UNIVERSITY with a focus on simplifying and streamlining processes. 
+It offers a user-friendly approach to solving problems, ensuring accessibility and ease of use for all stakeholders. 
+The code demonstrates a commitment to clarity and efficiency, providing intuitive solutions to complex challenges. 
+With clear documentation and concise implementation, this code fosters collaboration and empowers users to navigate and contribute effectively. 
+It reflects a dedication to enhancing the developer experience and fostering a welcoming environment for innovation and growth.*/
+
+
+#include <iostream>
+
+
+
 #include <iostream>
 #include <cmath>
 
